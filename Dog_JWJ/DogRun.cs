3d6e17c247cd6d0744bceb3702ed0bcb9753d5d9
Jared-Jacobs <jjacobs3@bellarmine.edu>
2020-01-30 15:04:34 -0500

    final commit

---
+++ Dog_JWJ/DogRun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dog_JWJ
+{
+    class DogRun
+    {
+
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.setName("Rex");
+            dog.setAge(7);
+
+            Console.WriteLine("The dogs name is " + dog.getName());
+            Console.WriteLine(dog.getName() + " is " + dog.getAge() + " human years old.");
+            Console.WriteLine(dog.getName() + " is " + dog.getDogYears() + " dog years old.");
+            Console.WriteLine("The Dogs information: " + dog.ToString());
+
+        }
+    }
+}
+
+
